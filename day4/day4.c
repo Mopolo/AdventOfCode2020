9d351165933920914a8fd2db08a71fe35d4269d0
Nathan Boiron <nathan.boiron@gmail.com>
2020-12-16 14:37:49 +0100

    Day 4B

--- day4/day4.c
+++ day4/day4.c
@@ -59,53 +59,105 @@ void day4() {
         char *trimed = trim(line);
 
         if (*trimed == 0) {
-            printf("Passport (%d): ", lineNumber);
+//            printf("Passport (%d): ", lineNumber);
 
             unsigned fieldsNotCid = 0;
             unsigned quantity = size;
 
-            unsigned quantity_byr = 0;
-            unsigned quantity_iyr = 0;
-            unsigned quantity_eyr = 0;
-            unsigned quantity_hgt = 0;
-            unsigned quantity_hcl = 0;
-            unsigned quantity_ecl = 0;
-            unsigned quantity_pid = 0;
-            unsigned quantity_cid = 0;
+            unsigned valid_byr = 0;
+            unsigned valid_iyr = 0;
+            unsigned valid_eyr = 0;
+            unsigned valid_hgt = 0;
+            unsigned valid_hcl = 0;
+            unsigned valid_ecl = 0;
+            unsigned valid_pid = 0;
+            unsigned valid_cid = 0;
 
             while (quantity > 0) {
-                printf("%s:%s ", fields[quantity].key, fields[quantity].val);
+//                printf("%s:%s ", fields[quantity].key, fields[quantity].val);
 
                 if (strcmp(fields[quantity].key, "byr") == 0) {
-                    quantity_byr++;
+                    long year = strtol(fields[quantity].val, (char **)NULL, 10);
+
+                    if (year >= 1920 && year <= 2002) {
+                        valid_byr++;
+                    }
                 }
 
                 if (strcmp(fields[quantity].key, "iyr") == 0) {
-                    quantity_iyr++;
+                    long year = strtol(fields[quantity].val, (char **)NULL, 10);
+
+                    if (year >= 2010 && year <= 2020) {
+                        valid_iyr++;
+                    }
                 }
 
                 if (strcmp(fields[quantity].key, "eyr") == 0) {
-                    quantity_eyr++;
+                    long year = strtol(fields[quantity].val, (char **)NULL, 10);
+
+                    if (year >= 2020 && year <= 2030) {
+                        valid_eyr++;
+                    }
                 }
 
                 if (strcmp(fields[quantity].key, "hgt") == 0) {
-                    quantity_hgt++;
+                    size_t length = strlen(fields[quantity].val);
+
+                    char s[3];
+                    char v[length - 2];
+
+                    memcpy(s, &fields[quantity].val[length - 2], 3);
+                    s[2] = '\0';
+
+                    memcpy(v, &fields[quantity].val[0], length - 2);
+                    v[length - 2] = '\0';
+
+                    long val = strtol(v, (char **)NULL, 10);
+
+                    printf("HGT:%s %ld %lu\n", s, val, length);
+
+                    unsigned min = 150;
+                    unsigned max = 193;
+                    if (strcmp(s, "in") == 0) {
+                        min = 59;
+                        max = 76;
+                    }
+
+                    if (val >= min && val <= max) {
+                        valid_hgt++;
+                    }
                 }
 
                 if (strcmp(fields[quantity].key, "hcl") == 0) {
-                    quantity_hcl++;
+                    if (fields[quantity].val[0] == '#'
+                        && strlen(fields[quantity].val) == 7
+                    ) {
+                        valid_hcl++;
+                    }
                 }
 
-                if (strcmp(fields[quantity].key, "ecl") == 0) {
-                    quantity_ecl++;
+                if (strcmp(fields[quantity].key, "ecl") == 0
+                    && (
+                        strcmp(fields[quantity].val, "amb") == 0
+                        || strcmp(fields[quantity].val, "blu") == 0
+                        || strcmp(fields[quantity].val, "brn") == 0
+                        || strcmp(fields[quantity].val, "gry") == 0
+                        || strcmp(fields[quantity].val, "grn") == 0
+                        || strcmp(fields[quantity].val, "hzl") == 0
+                        || strcmp(fields[quantity].val, "oth") == 0
+                    )
+                ) {
+                    valid_ecl++;
                 }
 
                 if (strcmp(fields[quantity].key, "pid") == 0) {
-                    quantity_pid++;
+                    if (strlen(fields[quantity].val) == 9) {
+                        valid_pid++;
+                    }
                 }
 
                 if (strcmp(fields[quantity].key, "cid") == 0) {
-                    quantity_cid++;
+                    valid_cid++;
                 }
 
                 quantity--;
@@ -113,14 +165,13 @@ void day4() {
 
             printf("\n");
 
-            if (quantity_byr == 1
-                && quantity_iyr == 1
-                && quantity_eyr == 1
-                && quantity_hgt == 1
-                && quantity_hcl == 1
-                && quantity_ecl == 1
-                && quantity_pid == 1
-                && (quantity_cid == 1 || quantity_cid == 0)
+            if (valid_byr == 1
+                && valid_iyr == 1
+                && valid_eyr == 1
+                && valid_hgt == 1
+                && valid_hcl == 1
+                && valid_ecl == 1
+                && valid_pid == 1
             ) {
                 quantityValid++;
             }
